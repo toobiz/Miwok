23e725ca878f19600082be8ad8d6d45b9dce6213
Mike Tubis <michal.tubis@weblify.co>
2016-10-03 17:21:18 +0200

    for loop

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -31,13 +31,22 @@ public class NumbersActivity extends AppCompatActivity {
 
         LinearLayout rootView = (LinearLayout)findViewById(R.id.rootView);
 
-        int index = 0;
+//        int index = 0;
 
-        while (index < words.size()) {
+//        while (index < words.size()) {
+//            TextView wordView = new TextView(this);
+//            wordView.setText(words.get(index));
+//            rootView.addView(wordView);
+//            index++;
+//        }
+
+        for (int index = 0; index < words.size(); index++)
+
+        {
             TextView wordView = new TextView(this);
             wordView.setText(words.get(index));
             rootView.addView(wordView);
-            index++;
         }
+
     }
 }
