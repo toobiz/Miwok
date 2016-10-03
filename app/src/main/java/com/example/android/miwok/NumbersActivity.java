a8e9b2deebc47cf54277699d21f21270c15408fc
Mike Tubis <michal.tubis@weblify.co>
2016-10-04 00:51:25 +0200

    ListView

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -5,7 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutCompat;
 import android.util.Log;
+import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -29,24 +31,11 @@ public class NumbersActivity extends AppCompatActivity {
         words.add("nine");
         words.add("ten");
 
-        LinearLayout rootView = (LinearLayout)findViewById(R.id.rootView);
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, words);
 
-//        int index = 0;
+        ListView listView = (ListView) findViewById(R.id.list);
 
-//        while (index < words.size()) {
-//            TextView wordView = new TextView(this);
-//            wordView.setText(words.get(index));
-//            rootView.addView(wordView);
-//            index++;
-//        }
-
-        for (int index = 0; index < words.size(); index++)
-
-        {
-            TextView wordView = new TextView(this);
-            wordView.setText(words.get(index));
-            rootView.addView(wordView);
-        }
+        listView.setAdapter(itemsAdapter);
 
     }
 }
