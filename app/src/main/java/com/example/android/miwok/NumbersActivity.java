27ace9e94390de11504526d4dc7c10b4a4697afb
Mike Tubis <michal.tubis@weblify.co>
2016-10-24 22:03:32 +0200

    add ImageView

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -1,14 +1,8 @@
 package com.example.android.miwok;
 
-import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutCompat;
-import android.util.Log;
-import android.widget.ArrayAdapter;
-import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -18,20 +12,21 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_numbers);
+        setContentView(R.layout.word_list);
 
         ArrayList<Word> words = new ArrayList<Word>();
 
         words.add(new Word("one", "lutti"));
-        words.add(new Word("one", "otiiko"));
-        words.add(new Word("one", "tolookosu"));
-        words.add(new Word("one", "oyyisa"));
-        words.add(new Word("one", "massokka"));
-        words.add(new Word("one", "temmokka"));
-        words.add(new Word("one", "kenekaku"));
-        words.add(new Word("one", "kawinta"));
-        words.add(new Word("one", "wo'e"));
-        words.add(new Word("one", "na'aacha"));
+        words.add(new Word("two", "otiiko"));
+        words.add(new Word("three", "tolookosu"));
+        words.add(new Word("four", "oyyisa"));
+        words.add(new Word("five", "massokka"));
+        words.add(new Word("six", "temmokka"));
+        words.add(new Word("seven", "kenekaku"));
+        words.add(new Word("eight", "kawinta"));
+        words.add(new Word("nine", "wo'e"));
+        words.add(new Word("ten", "na'aacha"));
+
 
         WordAdapter adapter = new WordAdapter(this, words);
 
